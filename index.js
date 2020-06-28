8fe4fa1a8482b61709084581e8e33372a0301968
Richard Lewis <richardlewis@Richards-MBP.lan>
2020-06-28 15:56:48 +0100

    update returning 1 promise.all

--- index.js
+++ index.js
@@ -4,29 +4,33 @@ const aws = require('aws-sdk');
 
 // initialize google oauth credentenatials 
 let readCredentials = gOAuth.readOauthDetails('credentials.json')
-let authorized = gOAuth.authorize(readCredentials, getGfiles)
+let authorized = gOAuth.authorize(readCredentials, run)
+
+function run(auth){
+  console.log(`run time:`)
+  getGfiles(auth).then(result => {console.log(result)})
+}
 
 // get Google meta data on files and folders
-function getGfiles(auth) {
-  let getRootFolder = getGdriveList(auth, {corpora: 'user', 
-  fields: 'files(name, parents)', 
-  q: "'root' in parents and trashed = false and mimeType = 'application/vnd.google-apps.folder'"})
-
-  let getFolders = getGdriveList(auth, {corpora: 'user', 
-  fields: 'files(id,name,parents), nextPageToken', 
-  q: "trashed = false and mimeType = 'application/vnd.google-apps.folder'"})
-
-  let getFiles = getGdriveList(auth, {corpora: 'user', 
-  fields: 'files(id,name,parents, mimeType, fullFileExtension, webContentLink, exportLinks, modifiedTime), nextPageToken', 
-  q: "trashed = false and mimeType != 'application/vnd.google-apps.folder'"})
-
-  //getFiles.then(result => {console.log(result)})
-  //getFolders.then(result => {console.log(result)})
-  //getRootFolder.then(result => {console.log(result)})
-
-  const allGFiles = Promise.all([getFiles, getFolders, getRootFolder])
-  allGFiles.then(results => {return results})
-  allGFiles.catch(error => console.log(`Error in retriving a file reponse from Google Drive: ${error}`))
+const getGfiles = (auth) => {
+  try {
+    let getRootFolder = getGdriveList(auth, {corpora: 'user', 
+    fields: 'files(name, parents)', 
+    q: "'root' in parents and trashed = false and mimeType = 'application/vnd.google-apps.folder'"})
+  
+    let getFolders = getGdriveList(auth, {corpora: 'user', 
+    fields: 'files(id,name,parents), nextPageToken', 
+    q: "trashed = false and mimeType = 'application/vnd.google-apps.folder'"})
+  
+    let getFiles = getGdriveList(auth, {corpora: 'user', 
+    fields: 'files(id,name,parents, mimeType, fullFileExtension, webContentLink, exportLinks, modifiedTime), nextPageToken', 
+    q: "trashed = false and mimeType != 'application/vnd.google-apps.folder'"})
+  
+    return Promise.all([getFiles, getFolders, getRootFolder])
+  }
+  catch(error) {
+    return `Error in retriving a file reponse from Google Drive: ${error}`
+  }
 }
 
 const getGdriveList = async (auth, params) => {

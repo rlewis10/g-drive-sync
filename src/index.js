a66327c26d9c1af5ac5768bd153bbbaf6cdbff23
Richard Lewis <richardlewis@Richards-MBP.lan>
2020-08-05 20:56:53 +0100

    connect gstream to s3 upload

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const AWS = require('aws-sdk')
 const run = async () => {
 
   const gKeys = await gOAuth.get()
+  const drive = google.drive({ version: 'v3', auth: gKeys })
   const awsKeys = await gOAuth.read('./cred/awskeys.json').then(result => {return result})
 
   
-  //gFiles.getGFilePaths(gAuth).then(data => {
+  //gFiles.getGFilePaths(gKeys).then(data => {
   //  console.log(data)
   //})
   
-  const drive = google.drive({ version: 'v3', auth: gKeys })
   const fileId = '1bNr_ZM90fM0EnPcFPfdd2LnB7Z2Tts3LiQ'
 
 
@@ -29,37 +29,29 @@ const run = async () => {
 }
 
 // download gFile
-const getGFileContent = (drive, fileId) => {
-  return drive.files
-  .get({fileId: fileId, mimeType: "image/jpeg", alt: 'media'}, {responseType: 'stream'})
-  .then(res => {
-    return new Promise((resolve, reject) => {
-      
-      const filePath = './data/photo.jpg'
-      const dest = fs.createWriteStream(filePath)
-  
-      res.data
-        .on('end', () => {resolve(`Done downloading file`)})
-        .on('error', err => {reject(`Error downloading file ${err}`)})
-        .pipe(dest)
+const getGFileContent = (drive, fileId) => {  
+  return drive.files.get({fileId: fileId, mimeType: "image/jpeg", alt: 'media'}, {responseType: 'stream'})
+    .then(res => {
+      return new Promise((resolve, reject) => {
+        res.data
+          .on('end', () => {resolve(`Done downloading file`)})
+          .on('error', err => {reject(`Error downloading file ${err}`)})
+          .pipe(awsUpload())
+      })
     })
-  })
 }
 
-const awsUpload = async () => {
+const awsUpload = () => {
   let pass = new stream.PassThrough()
   let params = {
     Bucket: "rlewis-backup", // bucket-name
     Key: 'filePath.jpg', // file will be saved as bucket-name/[uniquekey.csv]
-    Body: pass  // file data 
+    Body: pass  // file data passed through stream
   } 
-  try {
-    let uploadPromise = await new AWS.S3().upload(params).promise()
-    console.log("Successfully uploaded data to bucket")
-  } 
-  catch (e) {
-    console.log("Error uploading data: ", e)
-  }
+  new AWS.S3().upload(params).promise()
+    .then(() => console.log(`Successfully uploaded data to bucket`))
+    .catch( err => console.log(`Error, unable to upload to S3: ${err}`))
+  return pass
 }
 
 run()

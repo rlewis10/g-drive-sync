c970ff4ab75ebbefd6beaf43914b116c5c75ee3f
Richard Lewis <richardlewis@Richards-MBP.lan>
2020-08-13 22:45:15 +0100

    iterate over files

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
   
-const gDocs = require('./getGDocs')
+const gdocs = require('./getGDocs')
 const aws = require('./awsUpload')
-
-let file2 =     {
-  "id": "0B1r2V6Rg_NByLVFqYnItU1FUREE",
-  "name": "tash.jpg",
-  "mimeType": "image/jpeg",
-  "parents": [
-      "0B1r2V6Rg_NByR0RDQUsyRWVoTU0"
-  ],
-  "webContentLink": "https://drive.google.com/uc?id=0B1r2V6Rg_NByLVFqYnItU1FUREE&export=download",
-  "modifiedTime": "2016-02-29T10:40:18.000Z",
-  "fullFileExtension": "jpg",
-  "path": [
-      "People",
-      "Family",
-      "Tash"
-  ]
-}
 
 const run = async () => {
 
   await aws.awsAuth()
 
-  //gDocs.getGPaths(gKeys).then(data => {
-  //  console.log(data)
-  //})
-
-  gDocs.getGFiles(file2)
-
-
+  gdocs.getGPaths()
+    .then(data => {
+      data.forEach(file => gDocs.getGFiles(file))
+    })
+    .then(console.log('Done!'))
+    
 }
 
 run()

eea5166b30f2f09bb5ff52cda76676ee8e0f8464
Richard Lewis <richardlewis@Richards-MBP.lan>
2020-08-21 03:23:48 +0100

    added queue

--- src/index.js
+++ src/index.js
@@ -2,22 +2,30 @@ const gdocs = require('./getGDocs')
 const aws = require('./awsUpload')
 const goath = require('./googleOAuth')
 const file = require('./fileIO')
+const {default: PQueue} = require('p-queue')
 
 let numFilesTotal
-let numFilesUploaded
-let numFilesErrored
 
 const run = async () => {
   await gdocs.getOAuth2Client()
   await aws.awsAuth()
   //await file.write(gdocs.getGPaths(), './data/gFiles.json')
-  
+
+  const queue = new PQueue({concurrency: 10})
+
+  queue.on('add', () => {
+    console.log(`Task is added.  Size: ${queue.size}  Pending: ${queue.pending}`);
+  })
+  queue.on('next', () => {
+    console.log(`Task is completed.  Size: ${queue.size}  Pending: ${queue.pending}`);
+  })
+
   const allfiles = await file.read('./data/sampleData.json')
   numFilesTotal = allfiles.length
   console.log(`Total number of fies in gDrive to backup: ${numFilesTotal}`)
 
   allfiles.map(file => {
-    gdocs.getGFiles(file)
+    queue.add(() => gdocs.getGFiles(file))
   })
 }
 

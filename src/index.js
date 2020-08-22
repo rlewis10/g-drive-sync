04abf00ed648f33d5a2a4dce565115f9a5147f72
Richard Lewis <richardlewis@Richards-MBP.lan>
2020-08-22 17:05:56 +0100

    add cron job

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const aws = require('./awsUpload')
 const goath = require('./googleOAuth')
 const file = require('./fileIO')
 const {default: PQueue} = require('p-queue')
-
-let numFilesTotal
+const cron = require('node-cron')
 
 const run = async () => {
+  let numFilesTotal
   await gdocs.getOAuth2Client()
   await aws.awsAuth()
   //await file.write(gdocs.getGPaths(), './data/gFiles.json')
@@ -20,13 +20,16 @@ const run = async () => {
     console.log(`Task is completed.  Size: ${queue.size}  Pending: ${queue.pending}`);
   })
 
-  const allfiles = await file.read('./data/sampleData.json')
+  const allfiles = await file.read('./data/gFiles.json')
   numFilesTotal = allfiles.length
-  console.log(`Total number of fies in gDrive to backup: ${numFilesTotal}`)
+  console.log(`Total number of files in gDrive to backup: ${numFilesTotal}`)
 
   allfiles.map(file => {
     queue.add(() => gdocs.getGFiles(file))
   })
 }
 
-run()
+cron.schedule('1 0 * * *', run(), {
+  scheduled: true,
+  timezone: 'Europe/London'
+})

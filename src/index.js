740a88a6bb944919e766261b8e5d234cd21f64e9
Richard Lewis <richardlewis@Richards-MBP.lan>
2020-10-17 20:16:59 +0100

    sync all data

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ const cron = require('node-cron')
 const run = async () => {
   await gdocs.getOAuth2Client()
   await aws.awsAuth()
-  //await file.write(gdocs.getGPaths(), './data/gFiles.json')
+  await file.write(gdocs.getGPaths(), './data/gFiles.json')
 
   const queue = new PQueue({concurrency: 10})
 
   queue.on('add', () => {
-    console.log(`Task is added.  Size: ${queue.size}  Pending: ${queue.pending}`);
+    console.log(`Task is added to queue.  Size: ${queue.size}  Pending: ${queue.pending}`);
   })
   queue.on('next', () => {
-    console.log(`Task is completed.  Size: ${queue.size}  Pending: ${queue.pending}`);
+    console.log(`Task is completed and removed from queue.  Size: ${queue.size}  Pending: ${queue.pending}`);
   })
 
-  const allfiles = await file.read('./data/sampleData.json')
+  const allfiles = await file.read('./data/gFiles.json')
   console.log(`Total number of files in gDrive to backup: ${allfiles.length}`)
 
   allfiles.map(file => {
